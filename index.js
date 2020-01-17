be38ffd5efee66811a3da729b662a7ab1c01b837
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-17 12:34:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+// Code your solution 
+function findMatching(arr, s) {
+  arr= arr.filter(function (ss) {
+    
+    return ss.toLowerCase() === s.toLowerCase();
+
+  })
+  return arr;
+}
+
+function fuzzyMatch  (arr, s) {
+    arr= arr.filter(function(ss){
+    return ss.toLowerCase().indexOf(s.toLowerCase()) === 0;
+  })
+  return arr;
+}
+  
+function matchName(arr,s) {
+  arr = arr.filter(function(v){
+    return v['name'] === s });
+    return arr;
+}
